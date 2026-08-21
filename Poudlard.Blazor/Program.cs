89ef6031e36eaf7c25cb6b214a549681ded89c3d
FanatuXx <antoine.duff@hotmail.com>
2026-08-21 16:38:22 +0200

    Test changement de maison

--- Poudlard.Blazor/Program.cs
+++ Poudlard.Blazor/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("Poudlard", c => { c.BaseAddress = new Uri("https//localhost:7050/api/"); });
-builder.Services.AddHttpClient("SPW", c => { c.BaseAddress = new Uri("https//spw.be/api/"); });
+builder.Services.AddHttpClient("Poudlard", c => { c.BaseAddress = new Uri("https://localhost:7050/api/"); });
+builder.Services.AddHttpClient("SPW", c => { c.BaseAddress = new Uri("https://spw.be/api/"); });
 
 await builder.Build().RunAsync();

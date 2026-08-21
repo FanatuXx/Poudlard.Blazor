e3ea00034fdda790ac8d8e47736dda9c48cc2e11
TMCognitic <thierry.morre@cognitic.be>
2026-08-21 09:35:30 +0200

    Add HttpClient Factory

--- Poudlard.Blazor/Program.cs
+++ Poudlard.Blazor/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("Poudlard", c => { c.BaseAddress = new Uri("https//localhost:7050/api/"); });
+builder.Services.AddHttpClient("SPW", c => { c.BaseAddress = new Uri("https//spw.be/api/"); });
 
 await builder.Build().RunAsync();
